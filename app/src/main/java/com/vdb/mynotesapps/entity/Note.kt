69efe09c2826811fefed25103603e0572d6653ba
Vandeballoy <mac@VDB-Macbook-Pro-M1.local>
2023-07-15 12:14:53 +0700

    Initial commit

---
+++ app/src/main/java/com/vdb/mynotesapps/entity/Note.kt
@@ -0,0 +1,13 @@
+package com.vdb.mynotesapps.entity
+
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Note(
+    var id: Int = 0,
+    var title: String? = null,
+    var description: String? = null,
+    var date: String? = null
+) : Parcelable
